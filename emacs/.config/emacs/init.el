5961cb6415d4d65267d787a7455ef5ec398113f9
aleksozolins <aleks@aleksozolins.com>
2023-01-16 22:37:43 -0500

    Corrections and updates to tab-bar-mode

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -229,7 +229,7 @@
 (rg-enable-default-bindings))
 
 ;; Settings for tab-bar-mode
-(setq tab-bar-mode 1)                                            ; Enable tab-bar-mode
+(tab-bar-mode t)                                                 ; Enable tab-bar-mode
 (setq tab-bar-new-tab-choice "*scratch*")                        ; Automatically switch to the scratch buffer for new tabs
 (setq tab-bar-new-tab-to 'rightmost)                             ; Make new tabs all the way to the right automatically
 (setq tab-bar-new-button-show nil)                               ; Hide the new tab button - use the keyboard
@@ -238,15 +238,15 @@
 (setq tab-bar-format '(tab-bar-format-tabs tab-bar-separator))   ; Get rid of the history buttons in the tab bar
 
 ;; keybindings
-(global-set-key (kbd "s-[") 'tab-bar-switch-to-prev-tab)
-(global-set-key (kbd "s-]") 'tab-bar-switch-to-next-tab)
+(global-set-key (kbd "s-{") 'tab-bar-switch-to-prev-tab)
+(global-set-key (kbd "s-}") 'tab-bar-switch-to-next-tab)
 (global-set-key (kbd "s-t") 'tab-bar-new-tab)
 (global-set-key (kbd "s-w") 'tab-bar-close-tab)
 
 ;; tab-bar-history-mode lets you step back or forwad through the window config history of the current tab
 (tab-bar-history-mode t)
-(global-set-key (kbd "M-[") 'tab-bar-history-back)
-(global-set-key (kbd "M-]") 'tab-bar-history-forward)
+(global-set-key (kbd "s-[") 'tab-bar-history-back)
+(global-set-key (kbd "s-]") 'tab-bar-history-forward)
 
 ;; Put the elfeed DB on my Dropbox so the state syncs accross machines
 (setq elfeed-db-directory "~/Dropbox/apps/elfeed")
