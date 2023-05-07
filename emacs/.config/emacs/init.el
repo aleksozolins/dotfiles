5ba0aca78b73b6420a3460c7193a0722265dd1f3
aleksozolins <aleks@aleksozolins.com>
2023-05-07 13:50:03 -0400

    Modus Operandi by default and fixed link

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -93,7 +93,7 @@
 	(t . (semilight 1.1))))
 
 ;; Load a Theme
-(load-theme 'modus-vivendi t)
+(load-theme 'modus-operandi t)
 
 ;; Set a hot-key for switching between light and dark theme
 (define-key global-map (kbd "<f5>") #'modus-themes-toggle)
