1939086a6b28d399b57500a68e8d9b54aace092a
aleksozolins <aleks@aleksozolins.com>
2023-09-08 11:08:05 -0400

    Move authinfo.gpg to private repo

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -104,7 +104,7 @@
 (global-set-key (kbd "M-o") 'other-window)    ; Move to the other window C-x o but also now M-o
 (global-set-key (kbd "M-i") 'imenu)           ; Invoke imenu. This replaces tab-to-tab-stop but what is that even?
 
-(setq auth-sources '("~/.authinfo.gpg"))
+(setq auth-sources '("~/.local/share/emacs/authinfo.gpg"))
 
 (setq bookmark-default-file
       (pcase system-type
