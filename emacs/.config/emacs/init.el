c02c63db0b058c6dac8bdec35b0921b926ef5312
aleksozolins <aleks@aleksozolins.com>
2024-05-10 07:51:53 -0400

    Fix my-denote-daily function

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -724,7 +724,7 @@ entries exist, prompt with completion for a choice between them.
 Else create a new file."
   (interactive)
   (let* ((today (format-time-string "%A %e %B %Y"))
-         (string (denote-sluggify today))
+         (string (denote-sluggify 'title today))
          (files (denote-directory-files-matching-regexp string)))
     (cond
      ((> (length files) 1)
